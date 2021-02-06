93450e856e50338d7e02c9f424989d473c937183
santiagoidu <sr_santiago@icloud.com>
2021-02-06 10:17:54 -0300

    Create - Node.js and MongoDB patient registration

--- back-end/server.js
+++ back-end/server.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const path = require('path')
 const { env } = require('process')
 const mongoose = require('mongoose')
+const routes = require('./src/routes')
 
 const app = express();
 const uri = "mongodb+srv://saudeHi:HaekZLeaJy4IJnPP@cluster0.mrzzz.mongodb.net/<dbname>?retryWrites=true&w=majority"
@@ -24,9 +25,7 @@ mongoose.connect(uri, {
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
-app.get('/', function(req, res){
-    res.json({message: "Hello Word"})
-});
+app.use(routes);
 
 app.listen(port, function(){
     console.log(`Server runing on port ${port}`)
---
+++ back-end/src/models/pacientes.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const DataSchema = new mongoose.Schema({
+    nome_paciente: String,
+    celular_paciente: Number,
+    data_nascimento: String,
+    status_ativo: Boolean,
+    email_paciente: String,
+    senha_paciente: String
+}, {
+    timestamps:true
+})
+
+DataSchema.pre('save', function(next){
+    if(!this.isModified('senha_paciente')){
+        return next()
+    }
+    this.senha_paciente = bcrypt.hashSync(this.senha_paciente, 10)
+    next()
+})
+
+const pacientes = mongoose.model('Pacientes', DataSchema)
+module.exports = pacientes
