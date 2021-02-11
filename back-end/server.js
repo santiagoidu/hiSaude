d931430b36df20d2aaca00b3c22e260c7d33d297
santiagoidu <sr_santiago@icloud.com>
2021-02-11 05:02:05 -0200

    DEPLOY HEROKU

--- back-end/server.js
+++ back-end/server.js
@@ -5,13 +5,14 @@ const path = require('path')
 const { env } = require('process')
 const mongoose = require('mongoose')
 const routes = require('./src/routes')
- 
+const {APP_URL} = require('./src/.ENV')
+
 const app = express();
-const uri = "mongodb+srv://saudeHi:HaekZLeaJy4IJnPP@cluster0.mrzzz.mongodb.net/hi-healtcare?retryWrites=true&w=majority"
-const port = process.env.PORT || 5000
+const uri = "mongodb+srv://saudeHi:HaekZLeaJy4IJnPP@cluster0.mrzzz.mongodb.net/<dbname>?retryWrites=true&w=majority"
+const port = APP_URL || 5000
 
 mongoose.connect(uri, { 
-    useUnifiedTopology: true, 
+    useUnifiedTopology: true,
     useNewUrlParser: true,
     useFindAndModify: false
 }, function (err){
@@ -25,7 +26,6 @@ mongoose.connect(uri, {
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
 app.use(routes);
 
 app.listen(port, function(){
