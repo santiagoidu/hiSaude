697eb6c56ab8b6a4560828ea1b8929545e758995
santiagoidu <sr_santiago@icloud.com>
2021-02-12 05:18:31 -0200

    test deploy fix

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Inicio from './pages/client/painel'
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <h1>Hi Healthcare</h1>
+      </header> */}
+      <Inicio />
+    </div>
+  );
+}
+
+export default App;
